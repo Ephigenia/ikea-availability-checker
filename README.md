792c3993c0b302d0739221bbfef9f5787cbaf839
Marcel Eichner <love@ephigenia.de>
2017-02-02 14:24:25 +0100

    test: add simple tests for availability

--- README.md
+++ README.md
@@ -11,6 +11,8 @@ Sometimes there is a high demand on products which then are unavailable in the I
 
 # Usage Examples
 
+The default reports prints the results as a human readable table.
+
 ## Specific product, store & country
 
     source/cli.js stock --store=155 --country=at S49903093
--- source/lib/iows.test.js
+++ source/lib/iows.test.js
@@ -23,6 +23,31 @@ describe('iows', function() {
           'http://www.ikea.com/de/iows/catalog/availability/40299687%2C123'
         );
       });
-    });
-  });
+    }); // url
+
+    describe('availability', function() {
+      describe('failed', function() {
+        it('calls the done callback with an error', function(done) {
+          let product = iows.country('xx').product('40299687');
+          product.availability(function(err, results) {
+            expect(err).not.to.be.empty;
+            expect(err.response).to.be.defined;
+            expect(err.response.statusCode).to.equal(404);
+            expect(results).to.be.empty;
+            done();
+          });
+        });
+      });
+      describe('successfull', function() {
+        it('returns an array of product availabilitites', function(done) {
+          let product = iows.country('de').product('40299687');
+          product.availability(function(err, results) {
+            expect(err).to.be.null;
+            expect(results).to.be.an('array').to.have.length.of.gte(2);
+            done();
+          });
+        });
+      });
+    }); // availability
+  }); // country product
 });
