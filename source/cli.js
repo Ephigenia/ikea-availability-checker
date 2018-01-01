d97117372dd2d6d52e7c580eb966a2ba6a053149
Ephigenia <love@ephigenia.de>
2018-01-01 22:55:10 +0100

    build: changing package description and used in cli descriptipn as well

--- source/cli.js
+++ source/cli.js
@@ -7,6 +7,7 @@ let pkg = require('./../package.json');
 
 program
   .version(pkg.version)
+  .description(pkg.description)
   .command('stock', 'check the availability of one or multiple products')
   .command('stores', 'list stores in a specific country')
   .parse(process.argv);
