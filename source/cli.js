da8bc52f78de85828c33a724b25fc5b508ce2d75
Ephigenia <love@ephigenia.de>
2020-09-02 07:08:55 +0200

    fix: sub--command store, stock found when installed globally #18

--- source/cli.js
+++ source/cli.js
@@ -8,6 +8,10 @@ let pkg = require('./../package.json');
 program
   .version(pkg.version)
   .description(pkg.description)
-  .command('stock', 'check the availability of one or multiple products')
-  .command('stores', 'list stores in a specific country')
+  .command('stock', 'check the availability of one or multiple products', {
+    executableFile: 'cli-stock',
+  })
+  .command('stores', 'list stores in a specific country', {
+    executableFile: 'cli-stores',
+  })
   .parse(process.argv);
