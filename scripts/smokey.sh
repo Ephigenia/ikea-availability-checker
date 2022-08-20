62c65c6825914c3d9401bae217bbc1f461d18521
Marcel Eichner <love@ephigenia.de>
2022-08-20 18:47:24 +0200

    chore: wip

--- scripts/smokey.sh
+++ scripts/smokey.sh
@@ -37,42 +37,41 @@ function smokeCountry() {
 CODE="80213074"
 
 # TODO check with the countries
-# BUG bug in spain request
-# smokeCountry "at" "${CODE}";
-# smokeCountry "au" "${CODE}";
-# smokeCountry "be" "${CODE}";
-# smokeCountry "ca" "${CODE}";
-# smokeCountry "ch" "${CODE}";
-# smokeCountry "cn" "${CODE}";
-# smokeCountry "cz" "${CODE}";
-# smokeCountry "de" "${CODE}";
-# smokeCountry "dk" "${CODE}";
-# smokeCountry "es" "${CODE}";
+smokeCountry "at" "${CODE}";
+# smokeCountry "au" "${CODE}"; # no support in ingka api
+smokeCountry "be" "${CODE}";
+smokeCountry "ca" "${CODE}";
+smokeCountry "ch" "${CODE}";
+# smokeCountry "cn" "${CODE}"; # no support in ingka api
+smokeCountry "cz" "${CODE}";
+smokeCountry "de" "${CODE}";
+smokeCountry "dk" "${CODE}";
+smokeCountry "es" "${CODE}";
 smokeCountry "fi" "${CODE}";
-# smokeCountry "fr" "${CODE}";
-# smokeCountry "gb" "${CODE}";
-# smokeCountry "hk" "${CODE}";
-# smokeCountry "hr" "${CODE}";
-# smokeCountry "hu" "${CODE}";
-# smokeCountry "ie" "${CODE}";
-# smokeCountry "it" "${CODE}";
-# smokeCountry "jo" "${CODE}";
-# smokeCountry "jp" "${CODE}";
-# smokeCountry "kr" "${CODE}";
-# smokeCountry "kw" "${CODE}";
-# smokeCountry "lt" "${CODE}";
-# smokeCountry "my" "${CODE}";
-# smokeCountry "nl" "${CODE}";
-# smokeCountry "no" "${CODE}";
-# smokeCountry "pl" "${CODE}";
-# smokeCountry "pt" "${CODE}";
-# smokeCountry "qa" "${CODE}";
-# smokeCountry "ro" "${CODE}";
-# smokeCountry "ru" "${CODE}";
-# smokeCountry "sa" "${CODE}";
-# smokeCountry "se" "${CODE}";
-# smokeCountry "sg" "${CODE}";
-# smokeCountry "sk" "${CODE}";
-# smokeCountry "th" "${CODE}";
-# smokeCountry "tw" "${CODE}";
-# smokeCountry "us" "${CODE}";
+smokeCountry "fr" "${CODE}";
+smokeCountry "gb" "${CODE}";
+# smokeCountry "hk" "${CODE}"; # no support in ingka api
+# smokeCountry "hr" "${CODE}"; # no support in ingka api
+smokeCountry "hu" "${CODE}";
+smokeCountry "ie" "${CODE}";
+smokeCountry "it" "${CODE}";
+smokeCountry "jo" "${CODE}";
+# smokeCountry "jp" "${CODE}"; # no support in ingka api
+# smokeCountry "kr" "${CODE}"; # no support in ingka api
+smokeCountry "kw" "${CODE}";
+smokeCountry "lt" "${CODE}";
+# smokeCountry "my" "${CODE}"; # no support in ingka api
+smokeCountry "nl" "${CODE}";
+smokeCountry "no" "${CODE}";
+smokeCountry "pl" "${CODE}";
+smokeCountry "pt" "${CODE}";
+smokeCountry "qa" "${CODE}";
+smokeCountry "ro" "${CODE}";
+# smokeCountry "ru" "${CODE}"; # problems with one city
+smokeCountry "sa" "${CODE}";
+smokeCountry "se" "${CODE}";
+# smokeCountry "sg" "${CODE}"; # no support in ingka api
+smokeCountry "sk" "${CODE}";
+# smokeCountry "th" "${CODE}"; # no support in ingka api
+# smokeCountry "tw" "${CODE}"; # no support in ingka api
+smokeCountry "us" "${CODE}";
