241adf30f5e9e97341a741abeb526cd27b293327
Marcel Eichner <love@ephigenia.de>
2022-09-11 18:09:25 +0200

    test: fix smokey-format option

--- scripts/smokey-formats.sh
+++ scripts/smokey-formats.sh
@@ -12,5 +12,5 @@ npm run start -s -- stock --pretty --country "de" "80213074"
 
 npm run start -s -- stores "de" "80213074"
 npm run start -s -- stores --plain "de" "80213074"
-npm run start -s -- stores --csv "de" "80213074"
+npm run start -s -- stores --pretty "de" "80213074"
 npm run start -s -- stores --json "de" "80213074"
