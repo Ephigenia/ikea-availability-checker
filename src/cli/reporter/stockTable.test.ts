ca87cfa815b372ccf77f50abf4cf4fdc88f4a9d3
Marcel Eichner <love@ephigenia.de>
2023-01-08 16:51:35 +0100

    chore: wip

--- src/cli/reporter/stockTable.test.ts
+++ src/cli/reporter/stockTable.test.ts
@@ -1,11 +1,23 @@
 import { daysUntil } from "./stockTable";
 
 describe('daysUntil', function() {
-  it('returns the number comparing with NOW', function() {
+  it('returns more than 1 day for days in the future', function() {
     const dates = [
-      new Date('2022-01-08T15:14:49.634Z'),
+      new Date('2030-01-08T15:14:49.634Z'),
     ];
     const result = daysUntil(dates[0])
     expect(result).toBeGreaterThanOrEqual(1);
+  });
+  it('returns 1 for tomorrow date', function() {
+    const dates = [
+      new Date(
+        (new Date).getFullYear(),
+        (new Date).getMonth(),
+        (new Date).getDate() + 1,
+        (new Date).getHours() + 1,
+      ),
+    ];
+    const result = daysUntil(dates[0])
+    expect(result).toBe(1);
   });
 });
